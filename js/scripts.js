176e3a4359b0dfd7135e601304532fc4db407e24
Victor Puentes <vmpuentes@gmail.com>
2017-07-20 01:25:46 -0700

    add initial global variables to business logic side of the scripts.js file.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//business Logic
+
+var currentGame = new game();
+var thePlayer = new player();
+
+game.prototype.submit = function() {
+  this.thiPlayer.score += this.roundTotal;
+}
+
+var player1result = 0;
+
+
+
+
+
+//User Logic
+$(document).ready(function(); {
+  $("#userInput").submit(function(event) {
+  event.preventDefault();
+  }
+}
