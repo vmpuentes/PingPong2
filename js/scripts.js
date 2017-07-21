39a648f546b1ef1786acf6f100f0e776ae56639f
vmpuentes <vmpuentes@gmail.com>
2017-07-21 15:00:44 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//business Logic
+//*business Logic
 var player1 = (function(name){
   this.name = player1("player1");
   this.currentTurn = currentTurn;
@@ -20,4 +20,45 @@ $(document).ready(function(); {
   $("#userInput").submit(function(event) {
   event.preventDefault();
   }
-}
+}//*
+                         
+//business Logic
+var countToInput = function(number) {
+  var countToInitialInput = [];
+  for (var i = 0; i< number; i++) {
+    countToInitialInput[i] = (i + 1);
+  }
+var currentGame = (function(initialInput, currentTurn) {
+var initialInputResult = countToInput(number);
+  for (var i = 0; i < initialInputResult.length; i++) {
+    if((countToInput[i] | 3) === true) {
+      countToInput[i] = "ping";
+    } else if ((countToInput[i] | 5) === true) {
+      countToInput[i] = "pong";
+    } else if ((countToInput[i] | 15) === true) {
+      countToInput[i] = "pingpong";
+    }
+  }
+
+return initialInputResult;
+
+// Game.prototype.submit = function() {
+//   this.thisPlayer.score += this.roundTotal;
+// }
+// Player.prototype.getScore = function() {
+//   return this.score;
+//
+// var player1Result = 0;
+
+//User Logic
+$(document).ready(function() {
+  $("#ping").submit(function(event) {
+    event.preventDefault();
+    var initialInput = $(this.initialInput).val();
+    var runResult = $(initialInputResult + countToInput).val();
+    $(".submit").append(runResult);
+    });
+  });
+});
+                         
+                         
